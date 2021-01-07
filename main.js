e14306670f83c3b7bc9bf402e4766cf9d518b355
ringkurahman <mrringkurahman@gmail.com>
2021-01-07 22:23:04 +0400

    Added electron reload functionality on dev invironment

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron')
-
+const path = require('path')
+const isDev = !app.isPackaged
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -14,7 +15,14 @@ function createWindow() {
     })
 
     win.loadFile('index.html')
-    win.webContents.openDevTools()
+    isDev && win.webContents.openDevTools()
+}
+
+
+if (isDev) {
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+    })
 }
 
 
