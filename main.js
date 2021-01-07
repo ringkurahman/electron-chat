c4754b00cc696485b45128ea4701f524dff251c0
ringkurahman <mrringkurahman@gmail.com>
2021-01-07 18:13:00 +0400

    Added window close and reopen functionality

--- main.js
+++ main.js
@@ -13,6 +13,19 @@ function createWindow() {
     })
 
     win.loadFile('index.html')
+    win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
