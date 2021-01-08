94984d33925f0e6dc54e7b8e86106f27a022db03
ringkurahman <mrringkurahman@gmail.com>
2021-01-08 19:31:34 +0400

    Initialize firebase firestore with dotenv file

---
+++ src/js/api/chats.js
@@ -0,0 +1,12 @@
+import db from '../db/firestore'
+
+
+const extractSnapshotData = snapshot =>
+    snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+        
+
+export const fetchChats = () => db
+    .collection('chats')
+    .get()
+    .then(extractSnapshotData)
+   
--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import JoinChatsList from '../components/JoinChatsList'
 import AvailableChatsList from '../components/AvailableChatsList'
 import TitleContainer from '../components/shared/TitleContainer'
+import { fetchChats } from '../api/chats'
 
 
 const Home = () => {
+
+  useEffect(() => {
+    fetchChats()
+  }, [])
 
     return (
       <div className="row no-gutters fh">
